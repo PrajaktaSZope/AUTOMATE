86bd11d2338525076887900a10e9d76b4f660310
PrajaktaSZope <prajaktaszope@gmail.com>
2025-03-25 16:45:00 +0530

    comments commit

---
+++ src/lab002.java
@@ -0,0 +1,13 @@
+public class lab002 {
+
+    public static void main(String[] args) {
+        System.out.println("Helloworld");
+
+        // Single comment
+
+        /**
+         this is multi comment
+         */
+        System.out.println("print the multi comment");
+    }
+}
